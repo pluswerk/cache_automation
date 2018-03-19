5239b65f766148269cccdc827bbc6273c207c4eb
Marcus Schwemer <marcus.schwemer@in2code.de>
2018-03-19 17:07:11 +0100

    [FEATURE] Add agent for moved pages
    If this agent is activated, the caches of all children of this page will
    be cleared, when it is moved via the page tree.

---
+++ Classes/Agents/PageMovedAgent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Pluswerk\Project\Agents;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+use TYPO3\CMS\Core\Database\QueryGenerator;
+use \Pluswerk\CacheAutomation\Agents\AbstractAgent;
+
+class PageMovedAgent extends AbstractAgent
+{
+    public function getExpiredPages(string $table, $uid, array $agentConfiguration, array $changedFields): array
+    {
+        $pagesUidList = [];
+        if ($table === 'pages') {
+            if ($this->pageMovedInTree($changedFields)) {
+                $queryGenerator = GeneralUtility::makeInstance(QueryGenerator::class);
+                $pages = $queryGenerator->getTreeList($uid, 99, 0, 1);
+                $pagesUidList = array_merge($pagesUidList, explode(',', $pages));
+            }
+        }
+
+        return $pagesUidList;
+    }
+
+    /**
+     * Returns true, if the page was moved via the page tree
+     *
+     * @param array $changedFields
+     * @return bool
+     */
+    protected function pageMovedInTree(array $changedFields): bool
+    {
+        return array_keys($changedFields) === ['uid'];
+    }
+}
